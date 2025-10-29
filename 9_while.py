ebdf038c97eaeb790d80abefc817682474f57d3e
kevin ramirez <kevinramirezm0298@gmail.com>
2025-10-28 20:45:17 -0500

    clase de algoritmo del 28/10/25
    ciclo while notas y multiplicar y sumar

---
+++ 9_while.py
@@ -0,0 +1,33 @@
+# estructura repetitiva (while->acumulador)
+
+# x = 1
+# while x <= 5:
+#     print(x)
+#     x = x+1
+
+# num1 = int(input("estudiante: "))
+
+# # Inicializar la variable acumuladora
+# suma_total = 0
+
+# # ingresa 5 notas 
+# x = 1
+# suma = 0
+# while x <= 5:
+#     numero = int(input(f"Ingrese la nota : "))
+#     suma = suma + numero
+#     x = x + 1
+# # resultado nota final
+# print(f"La suma total de las notas ingresados es: {suma}")
+
+numero = int(input("ingrese valor: "))
+x = 1 
+
+while x <= 10:
+   
+    multiplicar = numero * x
+    x = x + 1
+    print (f"{numero} * {x} = {multiplicar}")
+
+
+
