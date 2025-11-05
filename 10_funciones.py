a62c2d725c4e4802749dca6f1a819ddbf05475af
kevin ramirez <kevinramirezm0298@gmail.com>
2025-11-04 20:46:57 -0500

    clase 04/11/25

---
+++ 10_funciones.py
@@ -0,0 +1,13 @@
+
+def hacer_retangulo():
+    ancho=int(input("ingresa el ancho "))
+    altura=int(input("ingresa el ancho "))
+    hacer_retangulo = ancho * altura
+
+    print("resultado", hacer_retangulo)
+
+hacer_retangulo()
+hacer_retangulo()
+hacer_retangulo()
+    
+
